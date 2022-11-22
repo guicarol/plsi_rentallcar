dd0ed5c8386111ecb56b929c07eb5099c2dd32f9
JLucas220 <73337859+JLucas220@users.noreply.github.com>
2022-11-22 13:21:05 +0000

    .

--- backend/views/layouts/navbar.php
+++ backend/views/layouts/navbar.php
@@ -3,43 +3,8 @@
 use yii\helpers\Html;
 
 ?>
-<!-- Navbar
+
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-        </li>
-        -->
-      <!--  <?php if(array_keys(Yii::$app->authManager->getRolesByUser(Yii::$app->user->getId()))[0] == "admin") {?>
-
-            <li class="nav-item d-none d-sm-inline-block">
-                <?= Html::a('Users', ['/user/index'], ['data-method' => 'post', 'class' => 'nav-link'])?>
-            </li>
-        <?php } ?>
-        <li class="nav-item d-none d-sm-inline-block">
-            <?= Html::a('Localizações', ['/localizacao/index'], ['data-method' => 'post', 'class' => 'nav-link'])?>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <?= Html::a('Extras', ['/extra/index'], ['data-method' => 'post', 'class' => 'nav-link'])?>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <?= Html::a('Veiculos', ['/veiculo/index'], ['data-method' => 'post', 'class' => 'nav-link'])?>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <?= Html::a('TiposVeiculos', ['/tipo-veiculo/index'], ['data-method' => 'post', 'class' => 'nav-link'])?>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <?= Html::a('Imagens', ['/imagem/index'], ['data-method' => 'post', 'class' => 'nav-link'])?>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <?= Html::a('Seguros', ['/seguro/index'], ['data-method' => 'post', 'class' => 'nav-link'])?>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <?= Html::a('Pedidos de Asssitência', ['/assistencia/index'], ['data-method' => 'post', 'class' => 'nav-link'])?>
-        </li>
-    </ul>
-
 
     <ul class="navbar-nav ml-auto">
 
@@ -60,12 +25,11 @@ use yii\helpers\Html;
                     </div>
                 </form>
             </div>
-        </li>-->
+        </li>
 
         <li class="nav-item">
             <?= Html::a('<i class="fas fa-sign-out-alt"></i>', ['/site/logout'], ['data-method' => 'post', 'class' => 'nav-link']) ?>
         </li>
 
-   <!-- </ul>
+    </ul>
 </nav>
-navbar -->
