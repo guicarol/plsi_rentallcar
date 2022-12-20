38344668e3bdb5167d4bcdff14d1912efaafa5cc
JLucas220 <73337859+JLucas220@users.noreply.github.com>
2022-12-20 17:58:08 +0000

    teset

---
+++ frontend/tests/unit/AnaliseTest.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace frontend\tests\Unit;
+
+use common\models\Analise;
+use frontend\tests\UnitTester;
+
+class AnaliseTest extends \Codeception\Test\Unit
+{
+
+    protected UnitTester $tester;
+
+    public function testValidacao()
+    {
+        $analise = new Analise();
+
+        $analise->comentario = null;
+        $this->assertFalse($analise->validate(['comentario']));
+
+        $analise->classificacao = 'wqesda';
+        $this->assertFalse($analise->validate(['classificacao']));
+
+        $analise->data_analise = 1231231232;
+        //$this->assertFalse($analise->validate(['data_analise']));
+
+        $analise->profile_id = '21sd';
+        $this->assertFalse($analise->validate(['profile_id']));
+
+        $analise->comentario = 'TESTE';
+        $analise->classificacao = 4;
+        //$analise->data_analise = 2022-12-16;
+        $analise->profile_id = 7;
+
+
+
+        $this->assertTrue($analise->validate([$analise->comentario]));
+        $this->assertTrue($analise->validate([$analise->classificacao]));
+        $this->assertTrue($analise->validate([$analise->data_analise]));
+        $this->assertTrue($analise->validate([$analise->profile_id]));
+
+
+
+    }
+
+    public function testSavingÃnalise()
+    {
+        $analise = new Analise();
+        $analise->comentario = 'TESTE Analise';
+        $analise->classificacao = 5;
+        //$analise->data_analise = 2022-12-17;
+        $analise->profile_id = 7;
+        $analise->save();
+        $this->tester->seeRecord('common\models\Analise', ['comentario' => 'TESTE']);
+
+        $altera = $this->tester->grabRecord('common\models\Analise', ['comentario' => 'TESTE']);
+        $altera->comentario = 'miniteste';
+        $altera->save();
+        $this->tester->seeRecord('common\models\Analise', ['comentario' => 'miniteste']);
+        $this->tester->dontSeeRecord('common\models\Analise', ['comentario' => 'TESTE']);
+        $altera->delete();
+        $this->tester->dontSeeRecord('common\models\Analise', ['comentario' => 'miniteste']);
+
+    }
+}
