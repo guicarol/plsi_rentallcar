45f11d16e71876687363e6e1c8ca9eb22a29ccf5
2202414 <2202414@my.ipleiria.pt>
2023-01-03 14:00:11 +0000

    re

--- backend/tests/acceptance/testeCest.php
+++ backend/tests/acceptance/testeCest.php
@@ -14,11 +14,26 @@ class testeCest
     // tests
     public function tryToTest(AcceptanceTester $I)
     {
-        $I->amOnPage('rentallcar/backend/web/index');
+        //criar um user com o seu profile, reservar um carro, ver os detalhes aluguer, passar para a conta admin, gerar a fatura, voltar para a nova conta user e ver a fatura
+        $I->amOnPage('rentallcar/frontend/web/index');
+        $I->wait(3);
+        $I->click('Registo');
+        $I->wait(3);
+        $I->fillField('SignupForm[nome]', 'user');
+        $I->fillField('SignupForm[apelido]', 'teste');
+        $I->fillField('SignupForm[telemovel]', 999111999);
+        $I->fillField('SignupForm[nif]', 999111999);
+        $I->fillField('SignupForm[cartaConducao]', 999111999);
+
+
+
+        /*$I->amOnPage('rentallcar/backend/web/index');
         $I->fillField('LoginForm[username]', 'admin');
         $I->fillField('LoginForm[password]', 'admin12345');
-        $I->wait(5);
+        $I->wait(3);
         $I->click('LoginForm[button]');
-        $I->wait(5);
+        $I->wait(3);
+        $I->click('Detalhes aluguer');
+        $I->wait(3);*/
     }
 }
